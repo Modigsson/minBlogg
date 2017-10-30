2d9c2b92d949106dd0a89286d6fa5e8adccbedd5
Christoffer Modigsson <31646763+Modigsson@users.noreply.github.com>
2017-10-30 19:32:03 +0100

    syncronized repo with local work

---
+++ data.php
@@ -0,0 +1,44 @@
+<?php
+
+$posts = [
+  [
+    'title' => 'Lucifer',
+    'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+    'author' => ['name' => 'Tom Kapinos', 'ID' => 0],
+    'date' => '2015-01-25',
+    'image' => 'https://vignette.wikia.nocookie.net/lucifer/images/4/49/S2_promo_Lucifer_city_of_angels.jpg/revision/latest/scale-to-width-down/670?cb=20161004113209'
+  ],
+
+  [
+    'title' => 'Angels and Demons',
+    'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+    'author' => ['name' => 'Ron Howard', 'ID' => 1],
+    'date' => '2009-05-13',
+    'image' => 'https://cdn.empireonline.com/jpg/80/0/0/1000/563/0/north/0/0/0/0/0/t/films/13448/images/wjrTJhDQEqmmaEfbM2Qo8ibzvSS.jpg'
+  ],
+
+  [
+      'title' => 'Legion',
+      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+      'author' => ['name' => 'Tom Kapinos', 'ID' => 2],
+      'date' => '2010-01-22',
+      'image' => 'http://www.digitalinberlin.de/wordpress/wp-content/legion.jpg'
+  ],
+
+  [
+      'title' => 'Hellboy',
+      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+      'author' => ['name' => 'Tom Kapinos', 'ID' => 3],
+      'date' => '2015-01-25',
+      'image' => 'https://images-na.ssl-images-amazon.com/images/M/MV5BMTY3MDM0ODA3M15BMl5BanBnXkFtZTYwNDQyMzc2._V1_.jpg'
+  ],
+
+  [
+      'title' => 'Clash of the titans',
+      'content' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!',
+      'author' => ['name' => 'Tom Kapinos', 'ID' => 4],
+      'date' => '2010-04-14',
+      'image' => 'https://img.yescdn.ru/2015/11/12/cover/Clash2.jpg'
+  ]
+
+];
---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Movie Blog</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/blog-home.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+
+
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+      <div class="row">
+
+        <!-- Blog Entries Column -->
+        <div class="col-md-8">
+
+          <h1 class="my-4">Movie Blog
+            <small>by Modig</small>
+          </h1>
+
+
+          <?php include('data.php');
+            foreach ($posts as $post):
+           ?>
+
+
+          <div class="card mb-4">
+            <img class="card-img-top" src="<?= $post['image']?>">
+            <div class="card-body">
+              <h2 class="card-title"><?= $post['title']?> <small>by <?= $post['author']['name']?></small></h2>
+
+              <p class="card-text"><?= $post['content']?></p>
+            </div>
+
+            <div class="card-footer text-muted">
+              <p>Aired <?= $post['date']?></p>
+            </div>
+          </div>
+
+          <?php endforeach; ?>
+
+
+    <!-- Footer -->
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; Your Website 2017</p>
+      </div>
+      <!-- /.container -->
+    </footer>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+
+  </body>
+
+</html>
